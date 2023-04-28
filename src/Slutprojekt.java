c93b07be93bca889bebfa5fbf33c29deb9b37084
saibot0506 <tobias.backman@elev.ga.ntig.se>
2023-04-28 20:45:49 +0200

    Började kodning

--- src/Slutprojekt.java
+++ src/Slutprojekt.java
@@ -1,2 +1,59 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Scanner;
+
 public class Slutprojekt {
+    public static void main(String[] args) {
+
+    }
+
+    private static int MC(){
+        Scanner AIc = new Scanner(System.in);
+    }
+
+
+    int MC = 0;
+
+    int TMEC = 0;
+
+    int APUp1 = 0;
+
+    int APUp2 = 0;
+
+    int PPUp1 = 0;
+
+    int PPUp2 = 0;
+
+    int IPC = 0;
+
+    int IPS = 0;
+
+
+    private class ML implements MouseListener {
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+            //System.out.println("Click!");
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+
+        }
+    }
 }
+
