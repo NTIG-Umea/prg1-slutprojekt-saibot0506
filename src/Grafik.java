bbbac5803c014fb3624c1525bb17c093535bc6b5
saibot0506 <tobias.backman@elev.ga.ntig.se>
2023-05-05 12:13:10 +0200

    Fixade så att fönstret blev interaktivt och gjorde så att de flesta knapparna funkar till viss del

--- src/Grafik.java
+++ src/Grafik.java
@@ -1,7 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferStrategy;
+import java.util.TimerTask;
 
-public class Grafik extends Canvas {
+public class Grafik extends Canvas implements Runnable{
+    private BufferStrategy bs;
+
+    private boolean running = false;
+    private Thread thread;
 
     //MoneyCounter
     int MC = 0;
@@ -9,17 +17,21 @@ public class Grafik extends Canvas {
     //TotalMoneyEarnedCounter
     int TME = 0;
 
-    //ActivePowerUp  O = Owned
+    //ActivePowerUp  O = Owned  I = Income
     int APUp1 = 10;
     int OAPUp1 = 0;
+    int IAPUp1 = 1;
     int APUp2 = 1000;
     int OAPUp2 = 0;
+    int IAPUp2 = 100;
 
-    //PassivePowerUp  O = Owned
+    //PassivePowerUp
     int PPUp1 = 100;
     int OPPUp1 = 0;
+    int IPPUp1 = 5;
     int PPUp2 = 10000;
     int OPPUp2 = 0;
+    int IPPUp2 = 200;
 
     //IncomePerClick
     int IPC = 1;
@@ -30,32 +42,43 @@ public class Grafik extends Canvas {
     //Egg
     int Egg = 1000000;
 
+    private Rectangle clickBox = new Rectangle(475, 500, 250, 150);
+    private Rectangle left1 = new Rectangle(100, 100, 200, 145);
+    private Rectangle left2 = new Rectangle(100, 275, 200, 145);
+    private Rectangle right1 = new Rectangle(900, 100, 200, 145);
+    private Rectangle right2 = new Rectangle(900, 275, 200, 145);
+    private Rectangle egg = new Rectangle(1400, 800, 120, 90);
+
+
+
     public static void main(String[] args) {
         Grafik x = new Grafik();
+        x.start();
 
     }
     public Grafik() {
         JFrame frame = new JFrame();
         frame.add(this);
+        this.addMouseListener(new ML());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1200, 800);
         frame.setVisible(true);
     }
-    public void paint(Graphics g) {
-
+    public void draw(Graphics g) {
+        g.clearRect(0,0,getWidth(),getHeight());
         //Background
         DrawRectangle(0, 0, 1920, 1080, new Color(190, 175, 225), g);
 
         //Click box
-        DrawRectangle(475, 500, 250, 150, new Color(0, 0, 0), g);
+        DrawRectangle(clickBox.x, clickBox.y, clickBox.width, clickBox.height, new Color(0, 0, 0), g);
 
         //Passive upgrades (Left)
-        DrawRectangle(100, 100, 200, 145, new Color(0, 0, 0), g);
-        DrawRectangle(100, 275, 200, 145, new Color(0, 0, 0), g);
+        DrawRectangle(left1.x, left1.y, left1.width, left1.height, new Color(0, 0, 0), g);
+        DrawRectangle(left2.x, left2.y, left2.width, left2.height, new Color(0, 0, 0), g);
 
         //Active upgrades (Right)
-        DrawRectangle(900, 100, 200, 145, new Color(0, 0, 0), g);
-        DrawRectangle(900, 275, 200, 145, new Color(0, 0, 0), g);
+        DrawRectangle(right1.x, right1.y, right1.width, right1.height, new Color(0, 0, 0), g);
+        DrawRectangle(right2.x, right2.y, right2.width, right2.height, new Color(0, 0, 0), g);
 
         //Money and total clicks (Left)
         DrawRectangle(50, 525, 300, 50, new Color(0, 0, 0), g);
@@ -65,8 +88,8 @@ public class Grafik extends Canvas {
         DrawRectangle(850, 525, 300, 50, new Color(0, 0, 0), g);
         DrawRectangle(850, 600, 300, 50, new Color(0, 0, 0), g);
 
-        //Easter Egg (Hidden:P)
-        DrawRectangle(1400, 800, 120, 90, new Color(0, 0, 0), g);
+        //Easter Egg (Hidden :P )
+        DrawRectangle(egg.x, egg.y, egg.width, egg.height, new Color(0, 0, 0), g);
 
 
         DrawText(500, 590, g, "Click Here!", 40);
@@ -105,5 +128,118 @@ public class Grafik extends Canvas {
         g.setColor(new Color(255, 255, 255));
         g.setFont(new Font("TimesRoman", Font.PLAIN, s));
         g.drawString(t, x, y);
+    }
+    public void render() {
+        bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        // Rita ut den nya bilden
+        draw(g);
+
+        g.dispose();
+        bs.show();
+    }
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void run() {
+        double ns = 1000000000.0 / 25.0;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+
+            while(delta >= 1) {
+                // Uppdatera koordinaterna
+                update();
+                // Rita ut bilden med uppdaterad data
+                render();
+                delta--;
+            }
+        }
+        stop();
+    }
+    public void update() {
+
+    }
+
+    public class ML implements MouseListener {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+             if(e.getX() >= clickBox.x && e.getX() <= clickBox.x + clickBox.width && e.getY() >= clickBox.y && e.getY() <= clickBox.y + clickBox.height){
+                System.out.println("Click!");
+                MC += IPC;
+                TME += IPC;
+             }
+            if(e.getX() >= left1.x && e.getX() <= left1.x + left1.width && e.getY() >= left1.y && e.getY() <= left1.y + left1.height){
+                System.out.println("Purchase Complete!");
+                IPS += IPPUp1;
+                IPPUp1 *= 1.05;
+                OPPUp1++;
+                PPUp1 *= 1.15;
+                MC -= PPUp1;
+            }
+            if(e.getX() >= left2.x && e.getX() <= left2.x + left2.width && e.getY() >= left2.y && e.getY() <= left2.y + left2.height) {
+                System.out.println("Purchase Complete!");
+                IPS += IPPUp2;
+                IPPUp2 *= 1.05;
+                OPPUp2++;
+                PPUp2 *= 1.15;
+                MC -= PPUp2;
+            }
+            if(e.getX() >= right1.x && e.getX() <= right1.x + right1.width && e.getY() >= right1.y && e.getY() <= right1.y + right1.height) {
+                System.out.println("Purchase Complete!");
+                IPC += IAPUp1;
+                IAPUp1 *= 1.1;
+                OAPUp1++;
+                APUp1 *= 1.10;
+                MC -= APUp1;
+            }
+            if(e.getX() >= right2.x && e.getX() <= right2.x + right2.width && e.getY() >= right2.y && e.getY() <= right2.y + right2.height) {
+                System.out.println("Purchase Complete!");
+                IPC += IAPUp2;
+                IAPUp2 *= 1.05;
+                OAPUp2++;
+                APUp2 *= 1.15;
+                MC -= APUp2;
+            }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
     }
 }
--- src/Slutprojekt.java
+++ src/Slutprojekt.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Scanner;
